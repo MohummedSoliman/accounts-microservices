8bef9965b2f8edf62ceb2a1a6381d85a222dde88
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-24 14:32:04 +0200

    create base class for common entities

---
+++ src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by", insertable = false)
+    private String updatedBy;
+}
