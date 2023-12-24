26ad4b63d232771d9409b3215e2499a8c83d5f99
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-24 22:24:23 +0200

    create service implementation class

--- src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -1,2 +1,41 @@
-package com.eazybytes.accounts.service.impl;public class AccountServiceImpl {
+package com.eazybytes.accounts.service.impl;
+
+import com.eazybytes.accounts.constant.AccountsConstant;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements IAccountsService {
+
+    private CustomerRepository customerRepository;
+    private AccountsRepository accountsRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
+        Accounts newAccount = createNewAccount(savedCustomer);
+        accountsRepository.save(newAccount);
+    }
+
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+
+        long randomAccNumber = 1000000000l + new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstant.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstant.ADDRESS);
+        return newAccount;
+    }
 }
