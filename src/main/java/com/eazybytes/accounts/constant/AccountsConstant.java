aa5683ad693cb94ecb49f9740980c1854d543ee2
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-24 22:24:39 +0200

    create constant file

--- src/main/java/com/eazybytes/accounts/constant/AccountsConstant.java
+++ src/main/java/com/eazybytes/accounts/constant/AccountsConstant.java
@@ -1,2 +1,17 @@
-package com.eazybytes.accounts.constant;public class AccountsConstant {
+package com.eazybytes.accounts.constant;
+
+public class AccountsConstant {
+
+    private AccountsConstant(){
+
+    }
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "123 main street, montaza";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account Created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request Processed Successfully";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An Error Occurred.Please try again or contact Dev team";
 }
