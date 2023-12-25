c88afeb3fb5853b1fa4c772b3517f69724845df8
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-25 13:44:48 +0200

    Add swagger annotaion for more informative description about app and account controller

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,35 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microService REST API Documentation",
+				description = "EazyBank Accounts microservice API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Mohamed Ibrahim",
+						email = "mohamed@mail.com",
+						url = "http://mohamed.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://mohamed.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts MicroServices REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/eazybytes/accounts/controller/AccountController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountController.java
@@ -4,6 +4,10 @@ import com.eazybytes.accounts.constant.AccountsConstant;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -16,6 +20,10 @@ import org.springframework.web.bind.annotation.*;
 import static com.eazybytes.accounts.constant.AccountsConstant.MESSAGE_201;
 import static com.eazybytes.accounts.constant.AccountsConstant.STATUS_201;
 
+@Tag(
+        name = "CRUD REST API For Accounts in EazyBank",
+        description = "CRUD REST API  in EazyBank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -24,8 +32,16 @@ public class AccountController {
 
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "Create Accounts Rest API",
+            description = "REST API to create Customer & Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status Created"
+    )
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
         iAccountsService.createAccount(customerDto);
 
@@ -37,18 +53,41 @@ public class AccountController {
                 ));
     }
 
+    @Operation(
+            summary = "Fetch Accounts Details Rest API",
+            description = "REST API to fetch Customer & Account inside EazyBank"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
-                                                               @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
-                                                               String mobileNumber){
+                                                           @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
+                                                           String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return new ResponseEntity<>(customerDto, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update Account Details Rest API",
+            description = "REST API to update Customer & Account inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
-        if(isUpdated){
+        if (isUpdated) {
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(new ResponseDto(AccountsConstant.STATUS_200, AccountsConstant.MESSAGE_200));
@@ -59,12 +98,27 @@ public class AccountController {
         }
     }
 
+    @Operation(
+            summary = "Delete Account Details Rest API",
+            description = "REST API to Delete Customer & Account inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccount(@RequestParam
-                                                         @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
-                                                         String mobileNumber){
+                                                     @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
-        if (isDeleted){
+        if (isDeleted) {
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(new ResponseDto(AccountsConstant.STATUS_200, AccountsConstant.MESSAGE_200));
