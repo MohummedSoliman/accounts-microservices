05551e11fd660a92ec6863d0fdb38c5bb85bd40a
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-25 07:58:38 +0200

    create new class for catch resource not found exception

--- src/main/java/com/eazybytes/accounts/exception/ResourceNotFoundException.java
+++ src/main/java/com/eazybytes/accounts/exception/ResourceNotFoundException.java
@@ -1,2 +1,13 @@
-package com.eazybytes.accounts.exception;public class ResourceNotFoundException {
+package com.eazybytes.accounts.exception;
+
+import ch.qos.logback.core.boolex.EvaluationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue){
+        super(String.format("%s not found with the given input data %s : %s",resourceName, fieldName, fieldValue));
+    }
 }
