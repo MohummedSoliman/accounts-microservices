98c6c735e6a45f897f1b87538bb04c9192c47dde
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-25 08:25:54 +0200

    create method to find account by cusotmer id

--- src/main/java/com/eazybytes/accounts/controller/AccountController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.eazybytes.accounts.controller;
 
-import static com.eazybytes.accounts.constant.AccountsConstant.*;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
@@ -8,10 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.eazybytes.accounts.constant.AccountsConstant.MESSAGE_201;
+import static com.eazybytes.accounts.constant.AccountsConstant.STATUS_201;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -31,5 +30,11 @@ public class AccountController {
                         STATUS_201,
                         MESSAGE_201
                 ));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+        CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
+        return new ResponseEntity<>(customerDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -1,10 +1,13 @@
 package com.eazybytes.accounts.service.impl;
 
 import com.eazybytes.accounts.constant.AccountsConstant;
+import com.eazybytes.accounts.dto.AccountsDto;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.entity.Accounts;
 import com.eazybytes.accounts.entity.Customer;
 import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.exception.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountMapper;
 import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
@@ -27,7 +30,7 @@ public class AccountServiceImpl implements IAccountsService {
     public void createAccount(CustomerDto customerDto) {
         Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
         Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customer.getMobileNumber());
-        if(optionalCustomer.isPresent()){
+        if (optionalCustomer.isPresent()) {
             throw new CustomerAlreadyExistsException("This Mobile number is already exists");
         }
         customer.setCreatedAt(LocalDateTime.now());
@@ -37,7 +40,7 @@ public class AccountServiceImpl implements IAccountsService {
         accountsRepository.save(newAccount);
     }
 
-    private Accounts createNewAccount(Customer customer){
+    private Accounts createNewAccount(Customer customer) {
         Accounts newAccount = new Accounts();
         newAccount.setCustomerId(customer.getCustomerId());
 
@@ -48,5 +51,25 @@ public class AccountServiceImpl implements IAccountsService {
         newAccount.setCreatedAt(LocalDateTime.now());
         newAccount.setCreatedBy("Anonymous");
         return newAccount;
+    }
+
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Customer", "Mobile Number", mobileNumber)
+                );
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString()
+                        )
+                );
+
+        CustomerDto customerDto = CustomerMapper.mapToCustomerDto(customer, new CustomerDto());
+        AccountsDto accountsDto = AccountMapper.mapToAccountsDto(accounts, new AccountsDto());
+        customerDto.setAccountsDto(accountsDto);
+
+        return customerDto;
     }
 }
