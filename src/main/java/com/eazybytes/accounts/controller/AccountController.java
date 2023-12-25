1a820736dccd49e08c4161d6d48d1db8735028c5
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-25 07:30:32 +0200

    add create new account api

--- src/main/java/com/eazybytes/accounts/controller/AccountController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.eazybytes.accounts.controller;
 import static com.eazybytes.accounts.constant.AccountsConstant.*;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
 
-
+        iAccountsService.createAccount(customerDto);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
